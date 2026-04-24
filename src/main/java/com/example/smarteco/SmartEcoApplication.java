08a02e035b9b2eea1e8410eb3c31208e24b6a19e
Miguel Cifuentes <miguelcifuentes@ustavillavo.edu.co>
2026-04-24 09:07:41 -0500

    feat: inicialización del proyecto

---
+++ src/main/java/com/example/smarteco/SmartEcoApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarteco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEcoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smarteco/SmartEcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarteco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEcoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
